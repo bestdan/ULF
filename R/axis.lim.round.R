c903b9947331a4fe7ef35b0b9e34ed46082d20f9
Daniel Egan <dp.egan@gmail.com>
2013-08-01 17:58:13 -0400

    Updated axis.lim.round

--- R/axis.lim.round.R
+++ R/axis.lim.round.R
@@ -1,19 +1,27 @@
-#' Round a number in log space for use in nice axes. 
+#' Round a number for use in nice axes labels & spacing
 #' @description This function is useful for taking a max number from data, and converting to a "nice" round number. 
 #' @param x The number to be "nicified" for axes. 
+#' @param direction Does the number need to be rounded up (max), or down(min)
 #' @return Another number
 #' @keywords finance, portfolio, annualize, convenience
 #' @seealso Nothing. 
 #' @export
 #' @examples
-#' axis.lim.round(pi^seq(1,10,0.2))
+#' this.seq<- pi^seq(1,10,0.2)
+#' up<- axis.lim.round(this.seq, direction="up")
+#' down<-  axis.lim.round(this.seq, direction="down")
+#' plot(this.seq~seq(1,length(this.seq)),
+#'      ylim=c(min(down),max(up)))
+#' lines(up~seq(1,length(this.seq)),col="green")
+#' lines(down~seq(1,length(this.seq)),col="red")
 
-
-# Rounder - for making nice axis labels -----------------------------------
-axis.lim.round<-function(x) {
-  this.10<-trunc(log10(x)) #number of 10
-  out<-round(x/(10^this.10)+1,0)*(10^this.10)
-  return(out)   
+axis.lim.round<-function(x,direction="up") {
+  this.10<-10^trunc(log10(x)) #number of digits
+  if (direction=="up") {
+    out<- ceiling(x/this.10)*this.10
+  } else {
+    out<- floor(x/this.10)*this.10
+  }
+  return(out)     
 }
-
 
