150fbbab41038d165421a7e02631b99517ed3172
Daniel Egan <dp.egan@gmail.com>
2013-08-15 14:26:17 -0400

    fixed bug in rolling_returns.

--- R/rolling_return.R
+++ R/rolling_return.R
@@ -1,7 +1,7 @@
 #' Rolling correlation of a bunch of vars to an index.
 #' @description Calculates rolling geometric average returns over the specified window. Note, this is not straight cumulative, but average cumulative. 
 #' @param rets Vector of returns for the form 1.05 for a 5 percent return. 
-#' @param window The number of observations for data to be observed over. 
+#' @param this.window The number of observations for data to be observed over. 
 #' @param ret.period The number of periods per year the return is calculated over daily=365, monthly=12, etc..
 #' @return A matrix of correlations
 #' @keywords finance, portfolio, annualize, convenience
@@ -14,15 +14,14 @@
 #' #rolling_return(rets,2,12)
 
 
-rolling_return<-function(rets,window,ret.period=365){
-  this.len<-length(rets) #number of ret observations
-  retvec<-rep(NA,this.len) #holder for output
-  for (i in this.len:window) {   #decrement from number of rets to window
-    these.rets<-cumprod(rets[(i-window+1):i])
-    this.ret<-these.rets[window]  
-    retvec[i]<-this.ret^(ret.period/window)
+rolling_return <-function (rets, this.window, ret.period = 252) {
+  this.len <- length(rets)     # How many datapoints do we have? 
+  retvec <- rep(NA, this.len)  # Create the output vector. 
+  for (i in this.len:this.window) {  #Move backwards from the max period until the winow size. 
+    these.rets <- cumprod(rets[(i - this.window + 1):i])  # Cumprd up just that window.. 
+    this.ret <- these.rets[this.window]                   # Get the last observation
+    retvec[i] <- this.ret^(ret.period/this.window)
   }
   return(retvec)
 }
-
 
