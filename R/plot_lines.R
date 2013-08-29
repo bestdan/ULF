b3283cbf0df36877ac9ec2688bf451238e5bba45
Daniel Egan <dp.egan@gmail.com>
2013-08-29 10:03:05 -0400

    updated plot lines for rainbow, non-overlapping colors.

--- R/plot_lines.R
+++ R/plot_lines.R
@@ -7,18 +7,42 @@
 #' @seealso Nothing. 
 #' @export
 #' @examples
-#' obs<-20
-#' rets<-data.frame(a=seq(.7,1.4,length.out=obs),b=runif(obs),c=rnorm(obs))  
-#' rets$y<-sin(rets$a+rets$b)
-#' plot_lines(rets)
+#'  obs<-20
+#'  rets<-data.frame(a=seq(.7,1.4,length.out=obs),b=runif(obs),c=rnorm(obs))  
+#'  rets$y<-sin(rets$a+rets$b)
+#'  plot_lines(rets)
+#'  rets<-data.frame(matrix(runif(obs*20),ncol=20,nrow=obs))  
+#'  for (i in 1:ncol(rets)) {
+#'  rets[,i]<- rets[,i] + i
+#'  }
+#'  index<-seq(1,obs)
+#'  plot_lines(cbind(index,rets))
 
 plot_lines<-function(mat, ...) {
+  ncols<- ncol(mat) - 1
+  thisColors<-rainbow(ncols)
+#   
+#   argnames <- names(list(...))
+#   # check whether b is an argument
+#   if(!("col" %in% argnames)) {
+#     thisColors<-rainbow(ncols)
+#   }
+#   # check whether d is an argument
+#   if(!("d" %in% argnames)) {
+#     d <- 1
+#   }
+#   # return NA for b and d if specified, but don't set a value
+#   list(a=a, b=ifelse(exists("b"), b, NA), d=ifelse(exists("d"), d, NA),
+#        args=list(...))
+#   
   xmin<-min(mat[,1],na.rm=TRUE)
   xmax<-max(mat[,1],na.rm=TRUE)
   ymin<-min(mat[,-1],na.rm=TRUE)
   ymax<-max(mat[,-1],na.rm=TRUE)
-  plot(mat[,2]~mat[,1],ylim=c(ymin,ymax),xlim=c(xmin,xmax),type="l",col=1,...)
+  plot(mat[,2]~mat[,1],ylim=c(ymin,ymax),xlim=c(xmin,xmax),type="l",col=thisColors[1], ...)
   for(i in 3:ncol(mat)) {
-    lines(mat[,i]~mat[,1],col=i-1, ...)  
+    lines(mat[,i]~mat[,1],col=thisColors[(i-1)], ...)  
   }
 }
+
+
