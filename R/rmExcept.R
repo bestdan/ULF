dd392c2fde75bdfa3ea3bc84a0a4962f7800109a
Daniel P. Egan <dp.egan@gmail.com>
2013-10-24 10:37:18 -0400

    minor improvements.

--- R/rmExcept.R
+++ R/rmExcept.R
@@ -1,17 +1,31 @@
 #' Remove everything from the environment except X
 #'
-#' @param x A list of objects to not be removed, x= c("this","that)
+#' @param vars A list of objects to not be removed, x= c("this","that)
+#' @param envir What environment should they be removed from. 
 #' @return Null - operates on the environment. 
 #' @export
 #' @examples
 #' ls()
+#' oldvar <- "c"
 #' currentvars<-ls()
 #' this <-seq(1:10)
 #' that <- seq(2:20)
+#' other <- c(2,4)
 #' ls()
-#' rmExcept(x=c(currentvars, "this"))  #Removes 'that'. 
+#' rmExcept(c(currentvars,"this"))  #Removes 'that'. 
 #' ls()
 
-rmExcept<- function(x) {
-  rm(list=(ls()[-which(ls() %in% x)]))
-}
+rmExcept<-  function(vars, envir = .GlobalEnv) { 
+  # vars <- c(vars, "dnrm") 
+  keep <- match(x = vars, table = ls(envir = envir)) 
+  if(any(is.na(keep))) { 
+    stop(paste("Some of the variables were not found in", 
+               environmentName(envir))) 
+  } 
+  rm(list = ls(envir = envir)[-keep], envir = envir) 
+  # cat("Removed all but", length(keep), "objects from", 
+  #    environmentName(envir), fill = TRUE) 
+} 
+
+
+ 
