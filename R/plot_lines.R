1ab61ae5eafcb44dcf7a9481ada2c1467bb72185
Daniel P. Egan <dp.egan@gmail.com>
2013-12-06 10:56:18 -0500

    Updated plot_lines to accept a color argument.

--- R/plot_lines.R
+++ R/plot_lines.R
@@ -17,30 +17,20 @@
 #'  }
 #'  index<-seq(1,obs)
 #'  plot_lines(cbind(index,rets))
-
-plot_lines<-function(mat, ...) {
-  ncols<- ncol(mat) - 1
-  thisColors<-rainbow(ncols)
-#   
-#   argnames <- names(list(...))
-#   # check whether b is an argument
-#   if(!("col" %in% argnames)) {
-#     thisColors<-rainbow(ncols)
-#   }
-#   # check whether d is an argument
-#   if(!("d" %in% argnames)) {
-#     d <- 1
-#   }
-#   # return NA for b and d if specified, but don't set a value
-#   list(a=a, b=ifelse(exists("b"), b, NA), d=ifelse(exists("d"), d, NA),
-#        args=list(...))
-#   
-  xmin<-min(mat[,1],na.rm=TRUE)
-  xmax<-max(mat[,1],na.rm=TRUE)
-  ymin<-min(mat[,-1],na.rm=TRUE)
-  ymax<-max(mat[,-1],na.rm=TRUE)
-  plot(mat[,2]~mat[,1],ylim=c(ymin,ymax),xlim=c(xmin,xmax),type="l",col=thisColors[1], ...)
-  for(i in 3:ncol(mat)) {
-    lines(mat[,i]~mat[,1],col=thisColors[(i-1)], ...)  
+plot_lines<- function (mat, thisColors=NULL, ...) 
+{
+  if (is.null(thisColors)) {
+    ncols <- ncol(mat) - 1
+    thisColors <- rainbow(ncols)
+  }
+  xmin <- min(mat[, 1], na.rm = TRUE)
+  xmax <- max(mat[, 1], na.rm = TRUE)
+  ymin <- min(mat[, -1], na.rm = TRUE)
+  ymax <- max(mat[, -1], na.rm = TRUE)
+  plot(mat[, 2] ~ mat[, 1], ylim = c(ymin, ymax), xlim = c(xmin, 
+                                                           xmax), type = "l", col = thisColors[1], ...)
+  for (i in 3:ncol(mat)) {
+    lines(mat[, i] ~ mat[, 1], col = thisColors[(i - 1)], 
+          ...)
   }
 }
