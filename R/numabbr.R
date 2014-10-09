5a1b81ba90278a24da26edf1847cd7488f367f9c
Daniel P. Egan <dp.egan@gmail.com>
2014-10-09 11:05:43 -0400

    update to numabbr.

--- R/numabbr.R
+++ R/numabbr.R
@@ -1,32 +1,38 @@
 #' Abbreviate numbers
 #' @param x numeric vector to be converted to dollars
+#' @param r Number of digits after the decimal to round to.
 #' @return string Vector of formatted strings with abbreviations.
 #' @keywords finance economics formating 
-#' @seealso \code{\link{nchar}} which this function wraps
 #' @export
 #' @examples
 #' options(scipen=20)
-#' x<- 10^seq(1,11)*4
+#' x<- 10^seq(1,11)*4.567
 #' cbind(formatC(x,big.mark = ",",format = "f", digits=0), unlist(sapply(x, numabbr)))
+#' cbind(formatC(x,big.mark = ",",format = "f", digits=0), unlist(sapply(x, function(x) numabbr(x, r=0))))
 
 
-numabbr<- function(x) {
+numabbr<- function(x,r=2) {
   
   temp<- floor(log10(max(x)))
   
   if(temp<3) {
-    return(x)
+    return(round(x,r))
   }
+  
+  abbr<- function(y,e,r) {
+    round((x/ 10^e ),r)
+  }
+  
   temp<- switch(as.character(temp),
-                  "3"= paste0((x/ 10^3 ),"k"), 
-                  "4"= paste0((x/ 10^3),"k"),
-                  "5"= paste0((x/ 10^3),"k"),
-                  "6"= paste0((x/ 10^6),"mm"),
-                  "7"= paste0((x/ 10^6),"mm"),
-                  "8"= paste0((x/ 10^6),"mm"),
-                  "9"= paste0((x/ 10^9),"tr"),
-                  "10"= paste0((x/ 10^9),"tr"),
-                  "11"= paste0((x/ 10^9),"tr"))
+                  "3"= paste0(abbr(x,3,r),"k"), 
+                  "4"= paste0(abbr(x,3, r),"k"),
+                  "5"= paste0(abbr(x,3, r),"k"),
+                  "6"= paste0(abbr(x,6, r),"mm"),
+                  "7"= paste0(abbr(x,6, r),"mm"),
+                  "8"= paste0(abbr(x,6, r),"mm"),
+                  "9"= paste0(abbr(x,9, r),"tr"),
+                  "10"= paste0(abbr(x,9, r),"tr"),
+                  "11"= paste0(abbr(x,9, r),"tr"))
   return(temp)  
 }
 
