f78a896b494f491ecb89e108c6a27ff72dc8e250
Daniel P. Egan <dp.egan@gmail.com>
2015-01-10 10:12:35 -0500

    updating xignite functions to allow for YAML file input.

--- R/XgetDividends.R
+++ R/XgetDividends.R
@@ -1,22 +1,40 @@
-#' Get prices & returns from XIgnite API
-#'
+#' @title Get prices & returns from XIgnite API
+#' @author Daniel Egan
 #' @param symbol The symbol to be queries
 #' @param start_date The beginning of the historical date range
 #' @param end_date The end of the historical date range. Defaults to today. 
+#' @param YAMLfile A locally held yaml file with XIgnite Credentials. 
 #' @param token Your API Token  
 #' @param ShowURL Should the XIgnite API ULR be shown (helps with debugging)
 #' @return A dataframe of date and dividends
 #' @keywords finance portfolio annualize convenience
-#' @import timeSeries
+#' @import timeSeries yaml
 #' @seealso Nothing. 
 #' @export
 #' @examples
 #' # Note: No examples will work without a valid token. 
-#' # data<-XgetDividends("AGG","2004-01-01")
-#' # head(data,20)
+#' \dontrun{
+#' data<-XgetDividends("AGG","2004-01-01", 
+#'    YAMLfile = "~/Documents/Betterment/credentials//XIgniteCredentials.yaml")
+#' head(data,20)
+#' }
 
-XgetDividends<-function(symbol,start_date,end_date=as.character(Sys.Date()),token="NA",ShowURL=FALSE) {
-  require(timeSeries)
+
+XgetDividends<-function(symbol,
+                        start_date,end_date=as.character(Sys.Date()),
+                        YAMLfile=NULL, token=NULL,
+                        ShowURL=FALSE) {
+  
+  if(is.null(YAMLfile) & is.null(token)) {
+    stop("Need to supply eitehr YAMLfile or token.")
+  }
+  
+  # Grab credentials from YAML file if available
+  if(!is.null(YAMLfile)) {
+    temp<- yaml.load_file(YAMLfile)
+    token<- temp$XIgniteToken
+  }
+  
   if (nchar(token) <5) {
     stop("You need to supply a valid token.")
   }
@@ -33,6 +51,8 @@ XgetDividends<-function(symbol,start_date,end_date=as.character(Sys.Date()),toke
   if(token!="NA") this.url<-paste0(this.url,"&_Token=",token)
   if (ShowURL==TRUE) print(this.url)
   result<- read.csv(this.url)
+  
+  
   names(result)<- c("Date", "Value")
   result <- aggregate(Value~Date, data=result, sum)
   result <- subset(result, Value!=0)  
--- R/XgetSymbols.R
+++ R/XgetSymbols.R
@@ -1,55 +1,81 @@
-#' Get prices & returns from XIgnite API
-#'
+#' @title Get prices & returns from XIgnite API
+#' @author Daniel Egan
 #' @param symbol The symbol to be queries
 #' @param start_date The beginning of the historical date range
 #' @param end_date The end of the historical date range. Defaults to today. 
 #' @param adjtype The type of adjustment, if any, to be made. Potentially
-#' "None",
-#' "SplitOnly",
-#' "CashDividendOnly",
-#' "SplitAndProportionalCashDividend",
-#' "SplitAndCashDividend",
-#' "All"
-#' @param quotetype The type of data to be returns. Options are
-#' Last, 
-#' Open,
-#' High,
-#' Low,
-#' Volume,
-#' LastClose (the default),
-#' ChangeFromOpen,
-#' PercentChangeFromOpen,
-#' ChangeFromLastClose,
-#' PercentChangeFromLastClose,
+#' \itemize{
+#'   \item{None}
+#'   \item{SplitOnly}
+#'   \item{CashDividendOnly}
+#'   \item{SplitAndProportionalCashDividend}
+#'   \item{SplitAndProportionalCashDividend}
+#'   \item{SplitAndCashDividend}
+#'   \item{All}
+#' }
+#' @param quotetype The type of data to be returns. Options are: 
+#' \itemize{
+#'   \item{Last}
+#'   \item{Open}
+#'   \item{High}
+#'   \item{Low}
+#'   \item{Volume}
+#'   \item{Close (the default)}
+#'   \item{PercentChangeFromOpen}
+#'   \item{ChangeFromLastClose}
+#'   \item{PercentChangeFromLastClose}
+#' }
+#' @param YAMLfile A locally held yaml file with XIgnite Credentials. 
 #' @param token Your XIgnite API Token  
 #' @param ShowURL Should the XIgnite API ULR be shown (helps with debugging)
 #' @return A dataframe of date and values
-#' @keywords finance, portfolio, annualize, convenience
-#' @seealso Nothing. 
+#' @keywords XIgnite 
 #' @export
-#' @import timeSeries 
+#' @import timeSeries yaml
 #' @examples
-#' # Note: no examples will work without a token. 
-#' # data<-XgetSymbols("EEM", start_date="2013-01-01", adjtype="SplitOnly")
-#' # head(data,20)
+#' # Note: Examples will no work without a valid XIgnite token. 
+#' \dontrun{
+#' data<-XgetSymbols("EEM", start_date="2013-01-01", adjtype="SplitOnly", 
+#'    YAMLfile = "~/Documents/Betterment/credentials/XIgniteCredentials.yaml")
+#' head(data,20)
+#' }
 
 
+XgetSymbols<-function(symbol,
+                      start_date,end_date=as.character(Sys.Date()),
+                      adjtype,
+                      quotetype="LastClose",
+                      YAMLfile=NULL,
+                      token=NULL, ShowURL=FALSE) {
 
-XgetSymbols<-function(symbol,start_date,end_date=as.character(Sys.Date()),adjtype,quotetype="LastClose",token="NA", ShowURL=FALSE) {
-  require(timeSeries)
-  if (nchar(token) <5) {
-    stop("You need to supply a valid token.")
+  if(is.null(YAMLfile) & is.null(token)) {
+    stop("Need to supply eitehr YAMLfile or token.")
   }
+  
+  # Grab credentials from YAML file if available
+  if(!is.null(YAMLfile)) {
+    temp<- yaml.load_file(YAMLfile)
+    token<- temp$XIgniteToken
+  }
+   
+  # Validate token
+  if (nchar(token) <5) {
+    stop("Token does not appear to be valid.")
+  }
+  
+  # Validate AdjType
   if (adjtype %in% c("None","SplitOnly",
                      "CashDividendOnly","SplitAndProportionalCashDividend", "SplitAndCashDividend","All")==FALSE) {
     stop("Incorrect adjtype input. Fail.")
   }
+  
   
   if (quotetype %in% c("Last","Open","High","Low","Volume","LastClose","ChangeFromOpen",
                      "PercentChangeFromOpen","ChangeFromLastClose","PercentChangeFromLastClose")==FALSE) {
     stop("Incorrect quotetype input. Fail.")
     
   }
+  
   start_date<- as.numeric(unlist(strsplit(start_date,"-")))
   start_date<- paste(start_date[2],start_date[3],start_date[1],sep="/")
   end_date<- as.numeric(unlist(strsplit(end_date,"-")))
@@ -60,13 +86,17 @@ XgetSymbols<-function(symbol,start_date,end_date=as.character(Sys.Date()),adjtyp
                    "&IdentifierType=Symbol&AdjustmentMethod=",adjtype,"&StartDate=",start_date,"&EndDate=",end_date,
                    "&_DownloadFile=true&_fields=GlobalQuotes.Date,","GlobalQuotes.",quotetype,"&_csvflatten=true")
   if(token!="NA") this.url<-paste0(this.url,"&_Token=",token)
+  
   if (ShowURL==TRUE) print(this.url)
+  
   result<-read.csv(this.url)
-  #result<-subset(result,select=c(-X))
+  
   names(result)<-c("Date","Value")
+  
   result<-subset(result,Value!=0)
   row.names(result)<-result[,1]
   names(result)<-c("Date",symbol)
   result<-as.timeSeries(result)
+  
   return(result)
 }
